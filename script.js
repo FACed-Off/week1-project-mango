871f354de899615e5a18453ce2b7ac3c7df0cd1f
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-06-03 14:22:15 +0100

    feat: add name input validation relates #3

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 function validateForm() {
-    //Runs on clicking contact form submit button
-    nameValidation();
-    // companyValidation();
-    // emailValidation();
+  //Runs on clicking contact form submit button
+  nameValidation();
+  if (nameValidation()) {
+    return true;
+  } else {
+    return false;
+  }
+  // companyValidation();
+  // emailValidation();
 }
 
 function nameValidation() {
-    //Validates the name input
-    let nameValue, validationText, lettersOnly;
+  //Validates the name input
+  let nameValue, validationText, lettersOnly;
 
-    //Gets value of the input field with id="name"
-    validationText = "";
-    nameValue = document.getElementById("name").value;
-    lettersOnly = /\d/
+  //Gets value of the input field with id="name"
+  validationText = "";
+  nameValue = document.getElementById("name").value;
+  lettersOnly = /\d/;
 
-    if (lettersOnly.test(nameValue)) {
-        validationText = "Name input must not contain non alphabet characters"
-        return false;
-        //Do not submit information
-    }
-
-    //Adds the validation text to the DOM by editing the errorText paragraph
+  if (lettersOnly.test(nameValue)) {
+    validationText = "Name input must not contain non alphabet characters";
     document.getElementById("errorText").innerHTML = validationText;
+    return false;
+    //Do not submit information
+  } else {
+    return true;
+  }
+
+  //Adds the validation text to the DOM by editing the errorText paragraph
 }
 
 // function companyValidation() {
